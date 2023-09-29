c4889c9ef79599f05c59a072b28193c5e976d0a0
durgaprasad118 <durgaprasadachana1108@gmail.com>
2023-09-29 23:51:39 +0530

    add readme

--- Backend/config/connect.js
+++ Backend/config/connect.js
@@ -1,6 +1,8 @@
-import 'dotenv/config'
+import dotenv from 'dotenv'
+dotenv.config()
 import mongoose from 'mongoose'
 const DATABASE_URL = process.env.DATABASE_URL
+console.log(DATABASE_URL)
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(DATABASE_URL, {
--- Backend/index.js
+++ Backend/index.js
@@ -1,33 +1,31 @@
+import dotenv from 'dotenv'
+dotenv.config()
 
-import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
 
-import express from "express";
-import cors from "cors";
-
-const app = express();
-const port =  process.env.PORT
-app.use(express.json());
+const app = express()
+const port = process.env.PORT
+app.use(express.json())
 // MongoDB connection
-import connectToDatabase from "./config/connect.js";
-connectToDatabase();
+import connectToDatabase from './config/connect.js'
+connectToDatabase()
 
 // Middleware
-app.use(cors());
+app.use(cors())
 
-
-app.get('/',(req,res)=>{
-  res.json({message:'Hello'})
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello' })
 })
 
 // Routes
-import tasksRouter from "./router/tasks.js";
-import authRouter from "./router/auth.js";
+import tasksRouter from './router/tasks.js'
+import authRouter from './router/auth.js'
 
-app.use("/tasks", tasksRouter);
-app.use("/auth", authRouter);
+app.use('/tasks', tasksRouter)
+app.use('/auth', authRouter)
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
+  console.log(`Server is running on port ${port}`)
+})
